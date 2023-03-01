ebf20b6ac9a2b160da94bbbf2d96b5d0958f2320
Valentin Banegas <TheValenK@gmail.com>
2023-03-01 20:01:56 -0300

    Añadiendo test para validar la actualizacion de una mascota

--- cypress/e2e/CreatePet.cy.js
+++ cypress/e2e/CreatePet.cy.js
@@ -1,9 +1,13 @@
+
 describe("CreatePet",()=>{
-    const url = "petstore.swagger.io/v2/pet"
     const pet = require("../fixtures/PetsData.json").pet
 
       it("Deberia crear una mascota",()=>{
           cy.AddPet(pet)
           cy.GetPetForId(pet.id).its("body").should("deep.eq", pet)
+      })
+
+      after(()=>{
+        cy.DeletePet(pet.id)
       })
 })
---
+++ cypress/e2e/UpdatePet.cy.js
@@ -0,0 +1,18 @@
+describe("UpdatePet",()=>{
+
+    const pet = require("../fixtures/PetsData.json").pet
+    const newPet = require("../fixtures/PetsData.json").petStatus
+    //el status de pet y de newpet son diferentes
+    before(()=>{
+        cy.AddPet(pet)
+    })
+
+    it("Deberia actualizarse el status", ()=>{
+        cy.UpdatePet(newPet)
+        cy.GetPetForId(newPet.id).its("body").should("deep.eq", newPet)
+    })
+
+    after(()=>{
+        cy.DeletePet(pet.id)
+      })
+})
