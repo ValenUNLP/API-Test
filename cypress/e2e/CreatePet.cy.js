4f185fbe9e6489b7911105f06bdeade98feeef15
Valentin Banegas <TheValenK@gmail.com>
2023-03-01 19:53:03 -0300

    Fix names

--- cypress/e2e/CreatePet.cy.js
+++ cypress/e2e/CreatePet.cy.js
@@ -1,8 +1,8 @@
-describe("test",()=>{
+describe("CreatePet",()=>{
     const url = "petstore.swagger.io/v2/pet"
     const pet = require("../fixtures/PetsData.json").pet
 
-      it("test",()=>{
+      it("Deberia crear una mascota",()=>{
           cy.AddPet(pet)
           cy.GetPetForId(pet.id).its("body").should("deep.eq", pet)
       })
